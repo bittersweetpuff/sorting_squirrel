edbd37912dbd341263b1209661ede496ab8125a3
BittersweetPuff <biszkoptex@gmail.com>
2019-12-24 17:43:25 +0100

    Minor changes in insertion sort loop

--- src/insertion_sort.rs
+++ src/insertion_sort.rs
@@ -1,14 +1,9 @@
 ///Function returns sorted array of values using insertion sort
 pub fn insertion_sort<T: Ord>(array: &mut [T]){
         for i in 0..array.len() {
-                let mut j = i;
-                loop {
-                        if j == 0 {
-                                break;
-                        }
+                for j in (1..i + 1).rev() {
                         if array[j] < array[j-1] {
                                 array.swap(j, j-1);
-                                j -= 1;
                         }
                         else {
                                 break;
